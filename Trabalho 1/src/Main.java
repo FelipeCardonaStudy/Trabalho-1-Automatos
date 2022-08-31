ea6aca5b70afc894f034ed11b6fcbbb9d10d9f3f
Felipe Rava Cardona <feliperavacardona@hotmail.com>
2022-08-31 19:31:22 -0300

    esqueleto do projeto

---
+++ Trabalho 1/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Teste t = new Teste();
+        t.testando();
+
+//        ArrayList<Character> terminais = new ArrayList<>();
+//        ArrayList<Character> naoTerminais = new ArrayList<>();
+//        ArrayList<String> gramaticas = new ArrayList<>();
+//
+//        Scanner teclado = new Scanner(System.in);
+//        System.out.println("Digite os símbolos terminais do seu automato divido por vírgulas:");
+//        String terminalIn = teclado.nextLine();
+//
+//        for(int i = 0; i < terminalIn.length(); i = i+ 2){
+//            terminais.add(terminalIn.charAt(i));
+//        }
+//
+//        System.out.println("Digite os símbolos não terminais do seu automato divido por vírgulas:");
+//        String naoTerminalIn = teclado.nextLine();
+//
+//        for(int i = 0; i < naoTerminalIn.length(); i = i+ 2){
+//            naoTerminais.add(naoTerminalIn.charAt(i));
+//        }
+//
+//        System.out.println("terminais: " + terminais);
+//        System.out.println("não terminais: " + naoTerminais);
+//
+//        System.out.println("Por favor, digite a gramática do 'S' separado por '|': ");
+//        String gramS = teclado.nextLine();
+//
+//        for(int i = 0; i < terminais.size(); i++){
+//            System.out.println("Por favor, digite a gramatica do '" + terminais.get(i) + "' separada por '|':");
+//            String gram = teclado.nextLine();
+//            gramaticas.add(gram);
+//        }
+//
+//        System.out.println("gramatica A: " + gramaticas.get(0));
+    }
+}
