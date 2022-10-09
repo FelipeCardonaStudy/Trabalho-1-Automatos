8573c634190160084918d02638d2a03d4752da4d
Felipe Rava Cardona <feliperavacardona@hotmail.com>
2022-10-09 14:03:43 -0300

    parte da gramatica pronta

--- Trabalho 1 ideia 2/src/Gramatica.java
+++ Trabalho 1 ideia 2/src/Gramatica.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Gramatica {
     ArrayList<Character> terminais = new ArrayList<>();
     ArrayList<Character> naoTerminais = new ArrayList<>();
+    Character simboloInicial;
     ArrayList<String> gramatica = new ArrayList<>();
 
     public boolean adicionaTerminal(Character t){
@@ -37,4 +39,43 @@ public class Gramatica {
         return naoTerminais.remove(n);
     }
 
+    public void setSimboloInicial(Character c){
+        simboloInicial = c;
+    }
+
+    public Character getSimboloInicial(){
+        return simboloInicial;
+    }
+
+    public boolean adicionaGramatica(String gramatica){
+        String separadorDeCampo = ";";
+        String separadorIdentificador = ":";
+
+        ArrayList<String> producaoFinal = new ArrayList<>();
+        ArrayList<String> linhas = new ArrayList<>(Arrays.asList(separadorDeCampo));
+
+        if(simboloInicial == null || gramatica.charAt(0) != simboloInicial){
+            return false;
+        }
+
+        for(int i = 0; i < linhas.size(); i++){
+            String testa = linhas.get(i);
+            if(!testaNaoTerminal(testa.charAt(0)) || testa.charAt(1) != ':'){
+                return false;
+            }
+            
+        }
+
+        return true;
+    }
+
+    private boolean testaNaoTerminal(Character n){
+        for(int i = 0; i < naoTerminais.size(); i++){
+            char test = naoTerminais.get(i);
+            if(test == n){
+                return true;
+            }
+        }
+        return false;
+    }
 }
